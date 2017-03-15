da8573841da99d596dd3a273f37d2196b0aece47
Vinay Kumar G <vinay.g@mlab.loc>
2017-03-15 11:42:12 +0530

    first commit

---
+++ app/src/main/java/com/example/vinayg/tmdb/TabFragment3.java
@@ -0,0 +1,9 @@
+package com.example.vinayg.tmdb;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by vinay.g.
+ */
+public class TabFragment3 extends Fragment {
+}
---
+++ app/src/main/java/com/example/vinayg/tmdb/pagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.vinayg.tmdb;
+/**
+ * Created by vinay.g on 08-Feb-17.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class pagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public pagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                PopularScreenFragment tab1 = new PopularScreenFragment();
+                return tab1;
+            case 1:
+                TabFragment2 tab2 = new TabFragment2();
+                return tab2;
+            case 2:
+                TabFragment3 tab3 = new TabFragment3();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
