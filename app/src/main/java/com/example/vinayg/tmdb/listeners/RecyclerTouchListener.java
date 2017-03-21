4b62736ba7dd94d47e64567924114968b6e0a2bb
Vinay Kumar G <vinay.g@mlab.loc>
2017-03-21 17:28:13 +0530

    code analysis

--- app/src/main/java/com/example/vinayg/tmdb/listeners/RecyclerTouchListener.java
+++ app/src/main/java/com/example/vinayg/tmdb/listeners/RecyclerTouchListener.java
@@ -2,11 +2,9 @@ package com.example.vinayg.tmdb.listeners;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Toast;
 
 /**
  * Created by vinay.g.
@@ -14,16 +12,13 @@ import android.widget.Toast;
 
 
 public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener, GestureDetector.OnDoubleTapListener {
-
-    private Context mContext;
     private GestureDetector gestureDetector;
     private ClickListener clickListener;
     private RecyclerView mRecyclerView;
-    View mChild;
+    private View mChild;
 
     public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
         this.clickListener = clickListener;
-        this.mContext = context;
         this.mRecyclerView = recyclerView;
         gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
             @Override
@@ -75,8 +70,6 @@ public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener, 
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
-        Log.d(" called double tap"," called double tap ");
-        Toast.makeText(mContext,"ondoubleTap",Toast.LENGTH_SHORT);
         mChild = mRecyclerView.findChildViewUnder(e.getX(), e.getY());
         if (mChild!= null && clickListener != null) {
             clickListener.onDoubleClick(mChild,  mRecyclerView.getChildAdapterPosition(mChild));
